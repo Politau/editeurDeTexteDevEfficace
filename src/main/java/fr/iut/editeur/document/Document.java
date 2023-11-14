554af7c7ecd7d1f232bfcacb79830e32c2d1326f
Paul Ruiz <paul.ruiz01@etu.umontpellier.fr>
2023-11-14 14:59:20 +0100

    feur

--- src/main/java/fr/iut/editeur/document/Document.java
+++ src/main/java/fr/iut/editeur/document/Document.java
@@ -2,48 +2,59 @@ package fr.iut.editeur.document;
 
 public class Document {
 
+    // La chaîne de texte du document
     private String texte;
 
+    // Constructeur par défaut
     public Document() {
+        // Initialise la chaîne de texte avec une chaîne vide
         this.texte = "";
     }
-	
+
+    // Getter pour obtenir le texte du document
     public String getTexte() {
         return texte;
     }
 
+    // Setter pour définir le texte du document
     public void setTexte(String texte) {
         this.texte = texte;
     }
 
+    // Ajoute une chaîne de texte à la fin du document
     public void ajouter(String texte) {
         this.texte += texte;
     }
 
+    // Redéfinition de toString pour obtenir la représentation texte du document
     @Override
     public String toString() {
         return this.texte;
     }
 
+    // Remplace une partie du texte par une nouvelle chaîne
     public void remplacer(int start, int end, String remplacement) {
         String leftPart = texte.substring(0, start);
         String rightPart = texte.substring(end);
         texte = leftPart + remplacement + rightPart;
     }
 
+    // Convertit en majuscules une partie du texte
     public void majuscules(int start, int end) {
-        String Gpart = texte.substring(0, start);
-        String Dpart = texte.substring(end);
-        String modif = texte.substring(start,end).toUpperCase();
-        texte = Gpart + modif + Dpart;
+        String leftPart = texte.substring(0, start);
+        String rightPart = texte.substring(end);
+        String modif = texte.substring(start, end).toUpperCase();
+        texte = leftPart + modif + rightPart;
     }
 
+    // Efface une partie du texte
     public void effacer(int start, int end) {
-        String Gpart = texte.substring(0, start);
-        String Dpart = texte.substring(end);
-        texte = Gpart + "" + Dpart;
+        String leftPart = texte.substring(0, start);
+        String rightPart = texte.substring(end);
+        texte = leftPart + "" + rightPart;
     }
 
+    // Efface complètement le texte du document
     public void clear(){
         texte= "";
     }
